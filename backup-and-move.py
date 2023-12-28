215b5354b88860ef06670427159c86af9d8abab0
Michael Sekatchev <64549194+msekatchev@users.noreply.github.com>
2023-12-27 19:16:38 -0800

    initial upload

---
+++ backup-and-move.py
@@ -0,0 +1,35 @@
+import os
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+local_folder_path = "/data/data/com.termux/files/home/storage/dcim/Camera" # folder from which to upload
+drive_folder_title = "Camera" # new folder to upload to
+final_folder_title = "../camera-roll" # where photos are moved after being uploaded
+
+# Authenticate and get GoogleDrive client
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth() # will require site login for approval
+drive = GoogleDrive(gauth)
+
+# create folder on the drive
+drive_folder = drive.CreateFile({'title': drive_folder_title, 'mimeType': 'application/vnd.google-apps.folder'})
+drive_folder.Upload()
+
+# Upload files to the created folder
+count = 1
+for root, dirs, files in os.walk(local_folder_path):
+    for file_name in files:
+        print(count, "\t", file_name)
+        count = count+1
+        
+        # Create empty file
+        local_file_path = os.path.join(root, file_name)
+        drive_file = drive.CreateFile({'title': file_name, 'parents': [{'id': drive_folder['id']}]})
+
+        # Set the content of the file
+        drive_file.SetContentFile(local_file_path)
+
+        # Upload the file
+        drive_file.Upload()
+
+        os.rename(local_folder_path+"/"+ file_name, final_folder_title+"/"+file_name) 
